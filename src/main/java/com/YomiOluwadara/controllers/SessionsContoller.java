b6ff7b28f4598a1f70f71bfcccbc246b9f1cef86
Yomi Oluwadara <40069286+oluwadara001@users.noreply.github.com>
2020-03-06 11:53:55 -0600

    begin creation of controllers in controllers package

---
+++ src/main/java/com/YomiOluwadara/controllers/SessionsContoller.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.YomiOluwadara.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author OO046152
+ * @RestController: makes class responds to incoming and outgoing as JSON end
+ *                  points
+ * @RequestMapping: specifies the route path/url
+ */
+@RestController
+@RequestMapping("/api/v1/sessions")
+public class SessionsContoller {
+
+}
