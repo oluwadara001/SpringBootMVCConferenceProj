dafa52beb060ac0f0cfe8842b13e85d9c59af973
Yomi Oluwadara <40069286+oluwadara001@users.noreply.github.com>
2020-03-06 21:58:34 -0600

    implented all the controllers, added jdbc driver to connect to pdagmin(postgres db)

--- src/main/java/com/YomiOluwadara/controllers/SessionsContoller.java
+++ src/main/java/com/YomiOluwadara/controllers/SessionsContoller.java
@@ -5,12 +5,14 @@ package com.YomiOluwadara.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.YomiOluwadara.models.Session;
@@ -26,7 +28,19 @@ import com.YomiOluwadara.repositories.SessionRepository;
  *             SessionRpeposity, the instance will be used to called the CRUD
  *             method the interface now have access to due to "extends"
  * @PostMapping :
- * @PathVariable : Take input and inject it as method parameter
+ * @PathVariable : Take input and inject it as method parameter method =
+ *               RequestMethod.DELETE (htpp delete) is needed if the operation
+ *               is not a GET or POST
+ * 
+ *               saveAndFlush: save changes and commit them to the database
+ * 
+ *               BeansUtils object:takes the existing session and copies in
+ *               incoming session data to it
+ * 
+ *               ignoreProperties: tells the BeansUtils what
+ *               properties/variables that wont be updated/replaced
+ * 
+ *               all CRUD methods implemented
  */
 
 @RestController
@@ -54,7 +68,29 @@ public class SessionsContoller {
 	@PostMapping
 	public Session create(@RequestBody final Session session) {
 		return sessionRepository.saveAndFlush(session);
-
 	}
 
+	// method that deletes a session given its id, provided no children sessions
+	// return method is void because record is being deleted
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable Long id) {
+		// ADD LOGIC to check for children method before deleting
+		sessionRepository.deleteById(id);
+	}
+
+	// find a session given its id,then store that id in Session variable (session)
+
+	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
+	public Session update(@PathVariable Long id, @RequestBody Session session) {
+		// taking the existing session and copies in incoming session data to it but
+		Session existingSession = sessionRepository.getOne(id);
+		// implementation of: BeanUtils.copyProperties(source, target,
+		// ignoreProperties);
+		BeanUtils.copyProperties(session, existingSession, "session_id");
+		// ignoringProperties (the session_id)
+		// ADD LOGIC to ensure all attributes of session are passed in( else they will
+		// be defaulted to null),otherwise return a 400 bad payload
+		return sessionRepository.saveAndFlush(existingSession);
+
+	}
 }
--- src/main/java/com/YomiOluwadara/controllers/SpeakersController.java
+++ src/main/java/com/YomiOluwadara/controllers/SpeakersController.java
@@ -5,10 +5,14 @@ package com.YomiOluwadara.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.YomiOluwadara.models.Speaker;
@@ -39,6 +43,28 @@ public class SpeakersController {
 	@RequestMapping("/{id}")
 	public Speaker get(@PathVariable Long id) {
 		return speakerRepository.getOne(id);
+	}
+
+	// creates a new speaker
+	@PostMapping
+	public Speaker create(@RequestBody Speaker speaker) {
+		return speakerRepository.saveAndFlush(speaker);
+	}
+
+	// method that deletes a speaker given their id
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable Long id) {
+		// ADD LOGIC to check for children method before deleting
+		speakerRepository.deleteById(id);
+	}
+
+	// method that takes a speaker id and update certain attributes/properties of
+	// the speaker
+	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
+	public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+		Speaker existingSpeaker = speakerRepository.getOne(id);
+		BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+		return speakerRepository.saveAndFlush(existingSpeaker);
 	}
 
 }
