bd5f0d6be98d7e0b77bdc187d63f45db0e1955fe
Yomi Oluwadara <40069286+oluwadara001@users.noreply.github.com>
2020-04-11 12:47:20 -0500

    implementing test case for find(id)

--- src/test/java/com/YomiOluwadara/conferencedemo/SessionsControllerTest.java
+++ src/test/java/com/YomiOluwadara/conferencedemo/SessionsControllerTest.java
@@ -3,8 +3,13 @@
  */
 package com.YomiOluwadara.conferencedemo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +20,9 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import com.YomiOluwadara.conferencedemo.controllers.SessionsContoller;
+import com.YomiOluwadara.conferencedemo.models.Attendee;
+import com.YomiOluwadara.conferencedemo.models.Session;
+import com.YomiOluwadara.conferencedemo.models.Speaker;
 import com.YomiOluwadara.conferencedemo.services.SessionsService;
 
 /**
@@ -27,6 +35,8 @@ import com.YomiOluwadara.conferencedemo.services.SessionsService;
  *         org.mockito.ArgumentMatchers.anyLong; that will be used in place of
  *         long id
  * 
+ *         NOTE: there are a lot of in-line comments
+ * 
  */
 
 class SessionsControllerTest {
@@ -34,25 +44,66 @@ class SessionsControllerTest {
 	@Nested
 	@DisplayName("testing the controller methods in session controller")
 	class SessionsServicesTests {
-		// used for injecting sessionsService that will be used to call methods in the
-		// SessionsService
+
+		// used for injecting SessionsContoller instance that will be used to call
+		// methods in the SessionsContoller class
 		@InjectMocks
 		SessionsContoller sessionsContoller;
-		// mock SessionService class as a class variable for the SessionsServicesTests
+		// mocks an instance of the SessionService that will be used to call methods in
+		// the SessionService class
 		@Mock
 		SessionsService sessionsService;
 
-		// initialize mock objects so objects are autowired
+		// bring in a variable of type session, so a Session object can be successfully
+		// created in this test class
+		Session session;
+
+		// making the session id a class variable so it could be shared among all the
+		// class member methods
+		final Long session_id = 0L;
+
 		@BeforeEach
 		void setup() throws Exception {
+			// needed from mockito API to initialize mocks and make them available in
+			// SessionController and SessiosSerives classes where they will be used
 			MockitoAnnotations.initMocks(this);
+
+			// creating an instance of the Sessions class so we can use an instance of the
+			// class Session to call the setter methods of the class that would be useful in
+			// setting each Session variables.These variables in the session class all needs
+			// to be valued so we can have a Session Object
+			Session session = new Session();
+			// session = new Session();
+
+			// using setters to set other variables for the object of a Session
+			session.setSession_name("test session");
+			session.setSession_description("I'm being tested");
+			session.setSession_length(60);
+			// creating a list object so we can add speakers and set value for the
+			// setSpeakers()
+			List<Speaker> speakers = new ArrayList<Speaker>();
+			// speakers.add(new Speaker().setFirst_name("first_name"));
+			session.setSpeakers(speakers);
+			List<Attendee> attendees = new ArrayList<Attendee>();
+			session.setAttendees(attendees);
+
 		}
 
 		@Test
 		@DisplayName("find one seesion given sesion id")
 		void findOneSessionTest() {
-
-			when(sessionsService.findOneSession(anyLong())).thenReturn(null);
+			// testing the "actual" result in the SessionsService class )- a session object
+			// ( having all the expected session attributes)
+			when(sessionsService.findOneSession(anyLong())).thenReturn(session);
+			Session sessionRestController = sessionsContoller.get(session_id);
+			// testing that user id is not null
+			assertNotNull(sessionRestController);
+			// assert that other Session variable from the session service are also equal to
+			// the values returned in the session controller (expected : session service ,
+			// actual session controller)
+			assertEquals(session_id, sessionRestController.getSession_id());
+			assertEquals(session.getSession_name(), sessionRestController.getSession_name());
+			assertEquals(session.getSession_description(), sessionRestController.getSession_description());
 
 		}
 
