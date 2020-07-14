e2767ffeaa87321f6946637bee99a53317e76cb7
Yomi Oluwadara <40069286+oluwadara001@users.noreply.github.com>
2020-07-13 21:04:45 -0500

    write live db junit test for find allSessions in the session controller class

--- src/test/java/com/YomiOluwadara/conferencedemo/SessionsControllerTestLiveDb.java
+++ src/test/java/com/YomiOluwadara/conferencedemo/SessionsControllerTestLiveDb.java
@@ -1,4 +1,20 @@
 package com.YomiOluwadara.conferencedemo;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 public class SessionsControllerTestLiveDb {
+	//test listAllSessions
+	static DataSource dataSource;
+	static Connection connection;
+	//set values for variables that will be inserted into the live db
+	private static final int session_id = 100;
+	private static final String session_name = "testSessionName";
+	private static final String session_description = "testSessionDesc";
+	private static final int session_length = 60;
+
+	private static final String sessionTableInsertionStatement =
+			"insert into sessions" + "(session_id,session_name,session_description,session_length)"
+					+ "values (?,?,?,?)";
+
 }
